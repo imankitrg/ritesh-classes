7a7285bba006b77a0aea23f76e7284c40acea9f1
ankit <imankitrg@gmail.com>
2026-06-02 23:09:21 +0530

    result page made

--- src/components/home/FacilitiesSection.jsx
+++ src/components/home/FacilitiesSection.jsx
@@ -105,7 +105,6 @@ export default function FacilitiesSection() {
 
         {/* Main Layout */}
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
-
           {/* Left Side Image */}
           <div className="relative">
 
